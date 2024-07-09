68a4c7abf412f587e9045fa51dbfb53cabd07c1a
Bandana Laishram <bandanalaishram@gmail.com>
2024-07-09 19:13:31 +0530

    Updating file name and loadblock function

--- federal/global-footer.js
+++ federal/global-footer.js
@@ -1,11 +1,3 @@
-const miloLibs = 'https://main--milo--adobecom.hlx.page/libs';
-const federalRepo = 'https://bootstraper--federal--adobecom.hlx.page';
-
-const defaultConfig =  {
-  miloLibs,
-  url: 'https://main--milo--adobecom.hlx.page/drafts/blaishram/my-footer',
-};
-
 const blockConfig = {
   name: 'global-footer',
   targetEl: 'footer',
@@ -13,9 +5,13 @@ const blockConfig = {
 }
 
 async function loadBlock() {
+  const miloLibs = 'https://stage--milo--adobecom.hlx.page/libs';
+  const federalRepo = 'https://bootstraper--federal--adobecom.hlx.page';
+
+  //Relative path can't be used, as the script will run on consumer's app
   const { bootstrapBlock } = await import(`${federalRepo}/scripts/utils.js`);
   const { locales } = await import(`${federalRepo}/constants/constants.js`);
-  const clientConfig = { locales, ...defaultConfig, ...window.fedsGlobalConfig }
+  const clientConfig = { locales, miloLibs, ...window.fedsGlobalConfig }
 
   bootstrapBlock(clientConfig, blockConfig);
 }
