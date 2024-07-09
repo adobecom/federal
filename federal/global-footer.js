8a08af69fb19e152534311d3977598da41fc185a
Bandana Laishram <bandanalaishram@gmail.com>
2024-07-09 22:15:01 +0530

    Adding localization supportt

--- federal/global-footer.js
+++ federal/global-footer.js
@@ -6,13 +6,20 @@ const blockConfig = {
 }
 
 async function loadBlock() {
-  const miloLibs = window.fedsGlobalConfig?.miloLibs || 'https://stage--milo--adobecom.hlx.page';
+  const { fedsGlobalConfig = {} } = window;
+  const miloLibs = fedsGlobalConfig.miloLibs || 'https://stage--milo--adobecom.hlx.page';
   const federalRepo = 'https://bootstraper--federal--adobecom.hlx.page';
 
   //Relative path can't be used, as the script will run on consumer's app
   const { bootstrapBlock } = await import(`${federalRepo}/scripts/utils.js`);
   const { locales } = await import(`${federalRepo}/constants/constants.js`);
-  const clientConfig = { locales, miloLibs: `${miloLibs}/libs`, ...window.fedsGlobalConfig }
+  const clientConfig = {
+    origin: federalRepo,
+    contentRoot: fedsGlobalConfig.contentRoot || '/federal/globalNav',
+    miloLibs: `${miloLibs}/libs`,
+    pathname: `/${fedsGlobalConfig.locale}`,
+    locales: fedsGlobalConfig.locales || locales,
+  }
 
   bootstrapBlock(clientConfig, blockConfig);
 }
