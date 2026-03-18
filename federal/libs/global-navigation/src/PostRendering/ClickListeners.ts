b2305a870872ad1c853010e5366a44411f749788
Raghav Sharma <raghavs@adobe.com>
2026-03-18 22:25:06 +0530

    Refactored the popup opening animation

--- federal/libs/global-navigation/src/PostRendering/ClickListeners.ts
+++ federal/libs/global-navigation/src/PostRendering/ClickListeners.ts
@@ -52,6 +52,38 @@ export const initClickListeners = (
 
 const animations = (gnav: HTMLElement): void => {
   const mainMenuButtons = [...gnav.querySelectorAll('.feds-gnav-items > li > button')];
+
+  // popover height animations
+  const popoverBackgroundRule =
+    [...document.adoptedStyleSheets.flatMap(sheet => [...sheet.cssRules])]
+    .find(rule => 
+          (rule as CSSStyleRule)?.selectorText === 'header.global-navigation nav::after');
+
+  mainMenuButtons.forEach(button => {
+    if (!popoverBackgroundRule) return;
+    const popup = button.nextElementSibling;
+    if (!popup) return;
+
+    // @ts-expect-error popup is a popover that has a toggle event
+    popup.addEventListener('toggle', (event: ToggleEvent) => {
+      if (event.newState === 'open') {
+        const newHeight = (popup as HTMLElement).offsetHeight ?? 0;
+        (popoverBackgroundRule as CSSStyleRule).style.height = `${newHeight + 72}px`;
+      } else {
+        (popoverBackgroundRule as CSSStyleRule).style.height = '100%';
+      }
+    });
+  });
+  window.addEventListener('resize', () => {
+    if (!popoverBackgroundRule) return;
+    const popup = gnav.querySelector('.feds-popup:popover-open');
+    if (!popup) return;
+    const newHeight = (popup as HTMLElement).offsetHeight ?? 0;
+    (popoverBackgroundRule as CSSStyleRule).style.height = `${newHeight + 72}px`;
+  });
+
+  // Mobile subscreen animations
+
   const fedsGnavItems = gnav.querySelector('.feds-gnav-items');
   mainMenuButtons.forEach(button => {
     button.addEventListener('click', () => {
