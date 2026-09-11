4dfe3b4f273251b3db9237c6bf71ec6443209274
Manasvi Agrawal <maagrawal@Manasvis-MacBook-Pro.local>
2026-09-11 12:14:04 +0530

    Add a placeholder for session notifications on the C2 GNAV

--- federal/libs/global-navigation/src/Main.ts
+++ federal/libs/global-navigation/src/Main.ts
@@ -288,6 +288,7 @@ export const renderGnavString = ({
   </ul>
   ${brandConciergeEnabled ? '<div class="feds-bc-wrapper"></div>' : ''}
   ${productCTA === null ? '' : productEntryCTA(productCTA)}
+  <div class="feds-notifications-wrapper"></div>
   ${unavEnabled ? '<div class="feds-utilities"></div>' : ''}
   ${breadcrumbs === null ? '' : renderBreadcrumbs(breadcrumbs)}
   <a href="#" class="trap-focus-gnav">.</a>
